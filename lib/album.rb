36159d76a3665b7d242454422441adad9d496e0d
zhbanton <zhbanton@gmail.com>
2014-06-17 16:22:51 -0400

    Album calculates duration of all tracks

--- lib/album.rb
+++ lib/album.rb
@@ -4,7 +4,15 @@ class Album
   def initialize(title, tracks)
     @title = title
     @track_list = tracks
-
   end
+
+  def duration
+    sum = 0
+    @track_list.each do |track|
+      sum += track.duration
+    end
+    sum
+  end
+
 
 end
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -2,15 +2,21 @@ require_relative '../lib/album.rb'
 require_relative '../lib/song.rb'
 
 describe Album do
+    let (:track1) { Song.new(title: "Don't Stop Believinnn", artists: ['Journey'], duration: 187, price: 0.99) }
+    let (:track2) { Song.new(title: "When the Lights go Down", artists: ['Journey'], duration: 190, price: 0.95) }
+    let (:tracks) { [track1, track2] }
+    let (:album) { Album.new("Journey's Greatest Hits", tracks) }
 
   describe 'attributes' do
-    track1 = Song.new(title: "Don't Stop Believinnn", artists: ['Journey'], duration: 187, price: 0.99)
-    track2 = Song.new(title: "When the Lights go Down", artists: ['Journey'], duration: 190, price: 0.95)
-    tracks = [track1, track2]
-    album = Album.new("Journey's Greatest Hits", tracks)
     it 'has a title and ordererd list of track objects' do
       expect(album.title).to eq "Journey's Greatest Hits"
       expect(album.track_list).to eq tracks
+    end
+  end
+
+  describe '#duration' do
+    it 'has a duration' do
+      expect(album.duration).to eq 377
     end
   end
 
